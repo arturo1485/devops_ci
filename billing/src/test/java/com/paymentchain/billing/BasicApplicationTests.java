6fe6b8ed0ee0d53948b3a18135a48c16c08bce92
arturo.torres <arturo.torres@galletasdonde.com>
2023-05-10 15:39:41 -0600

    add tst modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -13,6 +13,7 @@ public class BasicApplicationTests {
  @Test
  public void contextLoads() {
  	    String meessage= "default message cambio test devops";
+ 	    String meessage= "default ";
  	    Assert.assertNotNull(meessage);
  }
  
